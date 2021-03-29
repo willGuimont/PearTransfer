b73625258c9d1f6ca2a09759b2e947cfc273d13a
William Guimont-Martin <wil9876543210@hotmail.com>
2021-03-28 22:25:29 -0400

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,49 @@
 import React from 'react';
-import { Transfer } from './features/transfer/Transfer'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import { Transfer } from './features/transfer/Transfer'
 import { theme } from './theme/theme'
 import { ThemeProvider } from '@material-ui/styles';
+import { About } from './features/about/About';
 
 function App() {
   return (
-    <React.StrictMode>
-      <ThemeProvider theme={theme}>
-        <div className="App">
-          <header className="App-header">
-            <Transfer />
-          </header>
-        </div>
-      </ThemeProvider>
-    </React.StrictMode>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <React.StrictMode>
+              <ThemeProvider theme={theme}>
+                <div className="App">
+                  <header className="App-header">
+                    <Transfer />
+                  </header>
+                </div>
+              </ThemeProvider>
+            </React.StrictMode>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/features/about/About.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import styles from './About.module.css';
+
+export function About() {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+}
