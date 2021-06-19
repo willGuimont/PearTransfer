f93003befd2a02b85c9bab66144bd8a2f28f137e
William Guimont-Martin <wil9876543210@hotmail.com>
2021-06-18 23:00:08 -0400

    a bit of cleanup

--- src/features/transfer/Transfer.tsx
+++ src/features/transfer/Transfer.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { connectTo, newPeer, selectConnections, selectFiles, selectId, sendFile, } from './transferSlice';
+import React, {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {connectTo, newPeer, selectConnections, selectFiles, selectId, sendFile,} from './transferSlice';
 import styles from './Transfer.module.css';
-import { useLocation } from 'react-router-dom';
-import { Button } from "@material-ui/core";
-import { CopyToClipboard } from "react-copy-to-clipboard";
+import {useLocation} from 'react-router-dom';
+import {Button} from "@material-ui/core";
+import {CopyToClipboard} from "react-copy-to-clipboard";
 
 export function Transfer() {
     const id = useSelector(selectId);
@@ -31,43 +31,42 @@ export function Transfer() {
             dispatch(newPeer());
             setTimeout(() => {
                 if (urlId != null) {
-                    console.log(urlId);
                     dispatch(connectTo(urlId));
                 }
-              }, 500);
+            }, 500);
         }
     }, []);
 
     return (
         <div className={styles.transfer}>
-            <img src={process.env.PUBLIC_URL + '/pear2pear.png'} className="App-logo" alt="logo" width="20%" />
+            <img src={process.env.PUBLIC_URL + '/pear2pear.png'} className="App-logo" alt="logo" width="20%"/>
             <h1>Pear transfer</h1>
             <div>
                 {id}
-                <br />
-                <input type="text" placeholder="Other person's id" onChange={e => setPeerConnectionId(e.target.value)} />
-                <br />
+                <br/>
+                <input type="text" placeholder="Other person's id" onChange={e => setPeerConnectionId(e.target.value)}/>
+                <br/>
                 <Button variant="contained" color="primary" onClick={e => dispatch(connectTo(peerConnectionId))}>
                     Connect
                 </Button>
-                <br />
+                <br/>
                 <CopyToClipboard text={window.location.href + `?id=${id}`}>
                     <Button variant="contained" color="primary">
                         Get sharable link
                     </Button>
                 </CopyToClipboard>
-                <br />
+                <br/>
                 {connections.map((x, i) => <div key={i}>{x}</div>)}
-                <br />
-                <br />
+                <br/>
+                <br/>
                 <label>
                     File upload
-                    <br />
-                    <input type="file" onChange={e => handleFiles(e.target.files)} />
+                    <br/>
+                    <input type="file" onChange={e => handleFiles(e.target.files)}/>
                 </label>
-                <br />
+                <br/>
                 <p>Files</p>
-                <br />
+                <br/>
                 {files.map((x, i) =>
                     <div key={x.id}>
                         <a href={x.url} download={x.name}>{x.name}</a>
--- src/peer/peer.ts
+++ src/peer/peer.ts
@@ -9,7 +9,6 @@ let connections = new Map<string, Peer.DataConnection>();
 
 function onReceiveData(data: any, dispatch: ThunkDispatch<{}, {}, AnyAction>) {
     // TODO allow to receive other informations, such as other connected users
-    console.log(`Received data ${data}`);
     var blob = new Blob([data.file], {type: data.filetype});
     var url = URL.createObjectURL(blob);
     var fileName = data.filename;
@@ -19,12 +18,10 @@ function onReceiveData(data: any, dispatch: ThunkDispatch<{}, {}, AnyAction>) {
 }
 
 function registerNewConnection(conn: Peer.DataConnection, dispatch: ThunkDispatch<{}, {}, AnyAction>) {
-    console.log(`Registering ${conn.peer}`)
     let id = conn.peer;
 
     conn.on('data', data => onReceiveData(data, dispatch));
     conn.on('open', () => {
-        console.log('New connection opened');
     });
 
     connections.set(id, conn);
@@ -32,7 +29,6 @@ function registerNewConnection(conn: Peer.DataConnection, dispatch: ThunkDispatc
 }
 
 export function newPeer(dispatch: ThunkDispatch<{}, {}, AnyAction>) {
-    console.log('New peer');
     let id = generateId();
     peer = new Peer(id, {
         secure: true
@@ -61,17 +57,15 @@ function generateId(): string {
 export function connectTo(id: string, dispatch: ThunkDispatch<{}, {}, AnyAction>) {
     if (peer != undefined) {
         let conn = peer.connect(id);
-        console.log(`Connecting to ${id}`)
         registerNewConnection(conn, dispatch)
     }
 }
 
 export function sendFile(connectionIds: Array<string>, file: File, dispatch: ThunkDispatch<{}, {}, AnyAction>) {
-    // TODO add to self file list too
+    // TODO add to own file list too
     connectionIds.map(connId => {
         let conn = connections.get(connId);
         if (conn != undefined) {
-            console.log(`Sending file ${file.name} to ${connId}`);
             conn.send({
                 file: file,
                 filename: file.name,
